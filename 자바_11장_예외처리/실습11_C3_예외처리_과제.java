dfdb9a3026a327fa83d8ca2a4636c162b2538360
Young6575 <wnsdud11510@gmail.com>
2025-05-23 18:11:09 +0900

    예외처리 과제

--- 자바_11장_예외처리/실습11_C3_예외처리_과제.java
+++ 자바_11장_예외처리/실습11_C3_예외처리_과제.java
@@ -23,6 +23,40 @@ class Book implements Comparable<Book> {
 	private String author;
 	private int publicationYear;
 	private String isbn;
+
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getPublicationYear() {
+		return publicationYear;
+	}
+
+	public void setPublicationYear(int publicationYear) {
+		this.publicationYear = publicationYear;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
 	public int compareTo(Book b) { //추상 메소드를 구현
 		return title.compareTo(b.title);
 	}
@@ -37,7 +71,7 @@ class Book implements Comparable<Book> {
 	
 	@Override
 	public String toString() {
-		return String.format("도서명:\t%s\t저자:\t%s\t출판연도:\t%d\tISBN:\t%s", title, author, publicationYear, isbn);
+		return String.format("도서명:\t%s\t저자:\t%s\t출판연도:\t%d\tISBN:\t%s\n", title, author, publicationYear, isbn);
 	}
 
 
@@ -56,12 +90,16 @@ class Library {
 			}
 			books.add(book);
 		
-	
+
 	}
 
 	// 책 삭제 (빈 목록에서 삭제 시 UnderflowException 발생)
-	public Book removeBook() {
+	public void removeBook(Book book) throws UnderflowException {
 		
+		if (books.size() <= 0) {
+			throw new UnderflowException();
+		}
+		books.remove(book);					
 	}
 
 	public void printBooks(String msg) {
@@ -69,15 +107,28 @@ class Library {
 	}
 
 	public void sortBooksByTitle() {
-
+		books.sort((o1, o2) -> o1.compareTo(o2));
 	}
 
 	public void sortBooksByISBN() {
+		books.sort((o1, o2) -> o1.getIsbn().compareTo(o2.getIsbn()));
+	}
+
+	public void sortBooksByYear() {
 		
+		//books.sort((o1, o2) -> o1.getPublicationYear()==o2.getPublicationYear():0?(o1.getPublicationYear()>o2.getPublicationYear():1?-1)));
+		books.sort((o1, o2) -> {
+				if (o1.getPublicationYear()==o2.getPublicationYear())
+					return 0;
+				if (o1.getPublicationYear()>o2.getPublicationYear())
+					return 1;
+				return -1;
+		});
 	}
 
 	public Book searchBookByTitle(String title) {
-
+			
+		return null;
 	}
 }
 
@@ -88,8 +139,8 @@ public class 실습11_C3_예외처리_과제 {
 		try {
 			String s = new String("java");
 			System.out.println(s.length());
-			s = null;
-			System.out.println(s.length());
+			// s = null;
+			// System.out.println(s.length());
 			int[] arr = new int[2];
 			arr[2] = 10;
 		} catch (ArrayIndexOutOfBoundsException e1) {// 교재534
@@ -121,26 +172,30 @@ public class 실습11_C3_예외처리_과제 {
 			library.addBook(book3);
 			library.addBook(book4);
 			library.addBook(book5);
-			//library.addBook(book6);
+			library.addBook(book6);
+			
 			// 도서관의 용량을 초과하여 책을 추가 (예외 발생)
 		} catch (OverflowException e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();	
-		} catch (UnderflowException e) {
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-		}
+		} 
 		// 도서 목록 출력
 		library.printBooks("\n\n현재 도서 목록:");
 
-//		try {
-//			// 빈 도서관에서 책을 삭제 (예외 발생)
-//
-//		} catch () {
-//			
-//		}
-//
-//		// 최종 도서 목록 출력
-//		library.printBooks("\n최종 도서 목록:");
+		// 빈 도서관에서 책을 삭제 (예외 발생)
+		try {
+//			library.removeBook(book1);
+			
+			
+		} catch (UnderflowException e) {
+		System.out.println(e.getMessage());
+		e.printStackTrace();
+	}
+
+		library.sortBooksByISBN();
+		library.printBooks("\nISBN정렬 도서 목록:");
+		
+		// 최종 도서 목록 출력
+		library.printBooks("\n최종 도서 목록:");
 	}
 }
